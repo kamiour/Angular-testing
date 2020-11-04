cbb1e99e29c76e39391561bdbb3c363d22b8b504
Mykhailo Kazakov <mykhailo.kazakov@nixsolutions.com>
2020-11-04 12:09:09 +0200

    add calculator.service and courses.service tests

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
+
+describe("CoursesService", () => {
+  let coursesService: CoursesService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService],
+    });
+
+    coursesService = TestBed.get(CoursesService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  it("should retrieve all courses", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses returned");
+      expect(courses.length).toBe(12, "Incorrect number of courses");
+
+      const course = courses.find((course) => course.id === 12);
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = httpTestingController.expectOne("/api/courses");
+    expect(req.request.method).toEqual("GET");
+
+    req.flush({
+      payload: Object.values(COURSES),
+    });
+  });
+});
